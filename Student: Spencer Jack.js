8bc9a9b0d4b16fbb99bda19882bb9791d3f9666a
spencej4 <spencer.jack@alaskaair.com>
2018-06-12 08:43:19 -0700

    Capstone project idea for Spencer Jack

---
+++ Student: Spencer Jack.js
@@ -0,0 +1,18 @@
+// Student: Spencer Jack
+// Assignment: Capstone wireframing and elevator pitch
+
+
+// Build an React app which allows users to submit a search input and then receive JSON data, rendered to image, from Unsplash.com.
+	
+
+
+// This app shoudl be capable of:
+// 	-Setting state based on user input params
+// 	-Using that state to make an API call to Unsplash.com
+// 	-Create a 'card' (photo) for each returned image
+
+// 	Stretch goals:
+// 	-OAuth validation
+// 	-Allow user to tag favorites
+// 	-Create routes/ links to extra components which
+// 		-collect popular collections of photos based on a common theme	
