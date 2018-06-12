b7e47b536eaf731d082cb47bb45dd092493ed88c
spencej4 <spencer.jack@alaskaair.com>
2018-06-12 08:51:28 -0700

    Modified idea

--- Student: Spencer Jack.js
+++ Student: Spencer Jack.js
@@ -16,3 +16,20 @@
 // 	-Allow user to tag favorites
 // 	-Create routes/ links to extra components which
 // 		-collect popular collections of photos based on a common theme	
+
+// Week 1: 
+// 		-Write react-app basic scaffolding
+// 			-Call API
+// 			-Save returned json to state as data
+// 			-Pass data as props to a component that:
+// 				-renders image from url
+// Week 2
+// 		-Use user input param as query string for API call
+//  	-Integrate firebase into app for:
+//			-staging deployment
+// Week 3
+//		-UI cleanup/ polish
+//		-Experience requisite panic
+//		-Stretch goals:
+// 			-Firebase OAUTH?
+//			-Write components for 'popular' photo collections
